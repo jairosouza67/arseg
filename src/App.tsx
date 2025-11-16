459e97adc5d02865b5823df1a7159baa80f8e8f0
[dyad] <jairosouza67@gmail.com>
2025-11-15 22:42:37 -0300

    [dyad] wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { CartProvider } from "@/contexts/CartContext";
-import AppRouter from "@/router/AppRouter";
+import AppRouter from "./router/AppRouter";
 
 const queryClient = new QueryClient();
 
